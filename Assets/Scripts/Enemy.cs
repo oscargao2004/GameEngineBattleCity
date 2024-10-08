9b6db3e2ad6124adf02e98c08c4955d5ea4b717e
oscargao2004 <74120655+oscargao2004@users.noreply.github.com>
2024-10-08 09:49:42 -0400

    commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Listener
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void Execute()
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Listener.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Listener : MonoBehaviour
+{
+    public abstract void Execute();
+}
